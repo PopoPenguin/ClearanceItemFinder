205e9acc45bc5cc2bd0b971ff5c84517b66202d9
Jialor Cheung <jialorcheung@gmail.com>
2018-04-10 16:07:45 -0700

    setting up basic pojo

---
+++ src/main/java/com/application/model/Attributes.java
@@ -0,0 +1,39 @@
+package com.application.model;
+
+/**
+ * created by Jialor Cheung on 4/10/18
+ */
+
+public class Attributes {
+
+    String assemblyRequired;
+    String paperWoodIndicator;
+    String productUrlText;
+
+    public Attributes() {
+    }
+
+    public String getAssemblyRequired() {
+        return assemblyRequired;
+    }
+
+    public void setAssemblyRequired(String assemblyRequired) {
+        this.assemblyRequired = assemblyRequired;
+    }
+
+    public String getPaperWoodIndicator() {
+        return paperWoodIndicator;
+    }
+
+    public void setPaperWoodIndicator(String paperWoodIndicator) {
+        this.paperWoodIndicator = paperWoodIndicator;
+    }
+
+    public String getProductUrlText() {
+        return productUrlText;
+    }
+
+    public void setProductUrlText(String productUrlText) {
+        this.productUrlText = productUrlText;
+    }
+}
---
+++ src/main/java/com/application/model/Item.java
@@ -0,0 +1,336 @@
+package com.application.model;
+
+/**
+ * created by Jialor Cheung on 4/7/18
+ */
+
+public class Item {
+
+    Integer itemId;
+    Integer parentItemId;
+    String name;
+    Integer msrp;
+    Integer salePrice;
+    String upc;
+    String categoryPath;
+    String shortDescription;
+    String longDescription;
+    String brandName;
+    String thumbnailImage;
+    String mediumImage;
+    String largeImage;
+    String productTrackingUrl;
+    Boolean nenetySevenCentShipping;
+    String size;
+    String color;
+    Boolean marketplace;
+    Boolean shipToStore;
+    Boolean freeShipToStore;
+    String modelNumber;
+    String productUrl;
+    String customerRating;
+    Integer numReviews;
+    String customerRatingImage;
+    String cateogryNode;
+    Boolean bundle;
+    Boolean clearance;
+    Boolean preOrder;
+    String stock;
+    Boolean freight;
+    Attributes attributes;
+    String addToCartUrl;
+    String affiliateAddToCartUrl;
+    Boolean freeShippingOver50Dollars;
+    Boolean availableOnline;
+
+    public Item() {
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getParentItemId() {
+        return parentItemId;
+    }
+
+    public void setParentItemId(Integer parentItemId) {
+        this.parentItemId = parentItemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(Integer msrp) {
+        this.msrp = msrp;
+    }
+
+    public Integer getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(Integer salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getUpc() {
+        return upc;
+    }
+
+    public void setUpc(String upc) {
+        this.upc = upc;
+    }
+
+    public String getCategoryPath() {
+        return categoryPath;
+    }
+
+    public void setCategoryPath(String categoryPath) {
+        this.categoryPath = categoryPath;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getThumbnailImage() {
+        return thumbnailImage;
+    }
+
+    public void setThumbnailImage(String thumbnailImage) {
+        this.thumbnailImage = thumbnailImage;
+    }
+
+    public String getMediumImage() {
+        return mediumImage;
+    }
+
+    public void setMediumImage(String mediumImage) {
+        this.mediumImage = mediumImage;
+    }
+
+    public String getLargeImage() {
+        return largeImage;
+    }
+
+    public void setLargeImage(String largeImage) {
+        this.largeImage = largeImage;
+    }
+
+    public String getProductTrackingUrl() {
+        return productTrackingUrl;
+    }
+
+    public void setProductTrackingUrl(String productTrackingUrl) {
+        this.productTrackingUrl = productTrackingUrl;
+    }
+
+    public Boolean getNenetySevenCentShipping() {
+        return nenetySevenCentShipping;
+    }
+
+    public void setNenetySevenCentShipping(Boolean nenetySevenCentShipping) {
+        this.nenetySevenCentShipping = nenetySevenCentShipping;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getMarketplace() {
+        return marketplace;
+    }
+
+    public void setMarketplace(Boolean marketplace) {
+        this.marketplace = marketplace;
+    }
+
+    public Boolean getShipToStore() {
+        return shipToStore;
+    }
+
+    public void setShipToStore(Boolean shipToStore) {
+        this.shipToStore = shipToStore;
+    }
+
+    public Boolean getFreeShipToStore() {
+        return freeShipToStore;
+    }
+
+    public void setFreeShipToStore(Boolean freeShipToStore) {
+        this.freeShipToStore = freeShipToStore;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
+    public String getCustomerRating() {
+        return customerRating;
+    }
+
+    public void setCustomerRating(String customerRating) {
+        this.customerRating = customerRating;
+    }
+
+    public Integer getNumReviews() {
+        return numReviews;
+    }
+
+    public void setNumReviews(Integer numReviews) {
+        this.numReviews = numReviews;
+    }
+
+    public String getCustomerRatingImage() {
+        return customerRatingImage;
+    }
+
+    public void setCustomerRatingImage(String customerRatingImage) {
+        this.customerRatingImage = customerRatingImage;
+    }
+
+    public String getCateogryNode() {
+        return cateogryNode;
+    }
+
+    public void setCateogryNode(String cateogryNode) {
+        this.cateogryNode = cateogryNode;
+    }
+
+    public Boolean getBundle() {
+        return bundle;
+    }
+
+    public void setBundle(Boolean bundle) {
+        this.bundle = bundle;
+    }
+
+    public Boolean getClearance() {
+        return clearance;
+    }
+
+    public void setClearance(Boolean clearance) {
+        this.clearance = clearance;
+    }
+
+    public Boolean getPreOrder() {
+        return preOrder;
+    }
+
+    public void setPreOrder(Boolean preOrder) {
+        this.preOrder = preOrder;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public Boolean getFreight() {
+        return freight;
+    }
+
+    public void setFreight(Boolean freight) {
+        this.freight = freight;
+    }
+
+    public Attributes getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Attributes attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getAddToCartUrl() {
+        return addToCartUrl;
+    }
+
+    public void setAddToCartUrl(String addToCartUrl) {
+        this.addToCartUrl = addToCartUrl;
+    }
+
+    public String getAffiliateAddToCartUrl() {
+        return affiliateAddToCartUrl;
+    }
+
+    public void setAffiliateAddToCartUrl(String affiliateAddToCartUrl) {
+        this.affiliateAddToCartUrl = affiliateAddToCartUrl;
+    }
+
+    public Boolean getFreeShippingOver50Dollars() {
+        return freeShippingOver50Dollars;
+    }
+
+    public void setFreeShippingOver50Dollars(Boolean freeShippingOver50Dollars) {
+        this.freeShippingOver50Dollars = freeShippingOver50Dollars;
+    }
+
+    public Boolean getAvailableOnline() {
+        return availableOnline;
+    }
+
+    public void setAvailableOnline(Boolean availableOnline) {
+        this.availableOnline = availableOnline;
+    }
+}
