b960983ca28485db89e6bb37adf67a5d90f918e5
Jialor Cheung <jialorcheung@gmail.com>
2018-05-31 14:35:47 -0700

    repushing to repo

--- src/main/java/com/application/Application.java
+++ src/main/java/com/application/Application.java
@@ -3,6 +3,7 @@ package com.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
  */
 
 @SpringBootApplication
-public class Application {
+public class Application {//extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
