97b0650cc7fbccf30daca3166cad00d0d5a88b53
Jialor Cheung <jialorcheung@gmail.com>
2018-06-02 13:59:59 -0700

    revised controller for generic

--- src/main/java/com/application/controller/WalmartController.java
+++ src/main/java/com/application/controller/WalmartController.java
@@ -6,6 +6,7 @@ import com.application.service.WalmartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * created by Jialor Cheung on 5/20/18
@@ -18,7 +19,14 @@ public class WalmartController {
     WalmartService service;
 
     @RequestMapping("/clearance")
-    public Items getItems() {
-        return service.getItems();
+    public Items getItems(
+            @RequestParam (value = "categoryAPI", required = false, defaultValue = "/feeds/clearance")String categoryAPI,
+            @RequestParam (value = "isPublisherId", required = false)String isPublisherId,
+            @RequestParam (value = "format", required = false, defaultValue = "json")String format,
+            @RequestParam (value = "apiKey", required = true, defaultValue = "{apiKey}")String apiKey,
+            @RequestParam (value = "upc", required = false, defaultValue = "035000521019")String upc,
+            @RequestParam (value = "ids", required = false, defaultValue = "744021327")String ids
+    ) {
+        return service.getItems(categoryAPI, isPublisherId, format, apiKey, upc, ids);
     }
 }
