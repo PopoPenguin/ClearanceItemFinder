8f8eeba0716df3cbb34a6360ce7cedd2a560ac55
Jialor Cheung <jialorcheung@gmail.com>
2018-06-06 21:18:18 -0700

    adding counter and exceptions class

---
+++ src/main/java/com/application/exception/UnknownException.java
@@ -0,0 +1,9 @@
+package com.application.exception;
+
+/**
+ * created by Jialor Cheung on 6/6/18
+ */
+
+public class UnknownException {
+
+}
---
+++ src/main/java/com/application/model/Counter.java
@@ -0,0 +1,14 @@
+package com.application.model;
+
+/**
+ * created by Jialor Cheung on 6/6/18
+ */
+
+public class Counter {
+
+    private Integer count;
+    private Integer timer;
+    private Integer currentCount;
+    private Integer minusCount;
+
+}
