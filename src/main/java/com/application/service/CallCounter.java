6f29942ec06ab59e6cef4ee0cb22f12e2a7c4f48
Jialor Cheung <jialorcheung@gmail.com>
2018-06-06 21:45:36 -0700

    working on counter

--- src/main/java/com/application/service/CallCounter.java
+++ src/main/java/com/application/service/CallCounter.java
@@ -1,19 +1,25 @@
-package com.application.model;
+package com.application.service;
 
 /**
  * created by Jialor Cheung on 6/6/18
  */
 
-public class Counter {
+public class CallCounter {
 
     private Integer count;
     private Integer timer;
     private Integer currentCount;
-    private Integer minusCount;
+    private Integer totalCountDay;
 
     //current API key limits
     //5 calls/second
     //5,000 calls/day
 
+    public void TrackCount (Integer count){
+        currentCount += count;
+
+
+        return;
+    }
 
 }
