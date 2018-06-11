e747f57087852211cfe96bac04ee370c31a2ec8c
Jialor Cheung <jialorcheung@gmail.com>
2018-06-11 14:19:08 -0700

    verifying generic getForObject

--- src/main/java/com/application/service/WalmartService.java
+++ src/main/java/com/application/service/WalmartService.java
@@ -30,14 +30,14 @@ public class WalmartService {
 
 
     public Items getItems(String categoryAPI, String isPublisherId, String format, ApiKey apiKey, String upc, String ids){
-        String url = "http://api.walmartlabs.com/v1/feeds/clearance?format=json&apikey={apikey}&amp;categoryId=3944";
-        Items fullResponse = restTemplate.getForObject(url , Items.class);
-        return fullResponse;
+//        String url = "http://api.walmartlabs.com/v1/feeds/clearance?format=json&apikey={apikey}&amp;categoryId=3944";
+//        Items fullResponse = restTemplate.getForObject(url , Items.class);
+//        return fullResponse;
         //return restTemplate.getForObject("http://api.walmartlabs.com/v1/feeds/clearance?format=json&apikey={apikey}&amp;categoryId=3944", Items.class);
 
-//        GenericWalmartParams genericWalmartParams = new GenericWalmartParams( categoryAPI, isPublisherId, format, this.apiKey.getApiKey(), upc, ids);
-//        System.out.println(genericWalmartParams.domainParams());
-//        return restTemplate.getForObject(genericWalmartParams.domainParams(), Items.class);
+        GenericWalmartParams genericWalmartParams = new GenericWalmartParams( categoryAPI, isPublisherId, format, this.apiKey.getApiKey(), upc, ids);
+        System.out.println(genericWalmartParams.domainParams());
+        return restTemplate.getForObject(genericWalmartParams.domainParams(), Items.class);
 
     }
 
