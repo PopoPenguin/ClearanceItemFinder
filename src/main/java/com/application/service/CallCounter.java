022aedabdde738e7522629f63b686a55af31f65a
Jialor Cheung <jialorcheung@gmail.com>
2018-06-10 23:34:59 -0700

    looking into TokenBucket for rate limiting

--- src/main/java/com/application/service/CallCounter.java
+++ src/main/java/com/application/service/CallCounter.java
@@ -1,5 +1,7 @@
 package com.application.service;
 
+import org.springframework.util.StopWatch;
+
 /**
  * created by Jialor Cheung on 6/6/18
  */
@@ -16,12 +18,16 @@ public class CallCounter {
     //5,000 calls/day
 
     public void TrackCount (Integer count){
+
+        StopWatch stopWatch = new StopWatch();
+
+        //possibly use a TokenBucket to limit rate calls
+
         currentCount += count;
         //count gets added to total count
         //currentcount cannot exceed 5 /sec
         //totalcount cannot exceed 5000/day
         //reset totalcount at end of day
-
 
 
         return;
